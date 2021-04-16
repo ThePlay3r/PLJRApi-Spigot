ffcf8450590660630721b6c9927f0bcdc5886ce5
theplay3r <theplay3rko@gmail.com>
2021-04-16 15:06:57 +0200

    ignore this

--- src/main/java/me/pljr/pljrapispigot/utils/HologramsUtil.java
+++ src/main/java/me/pljr/pljrapispigot/utils/HologramsUtil.java
@@ -1,9 +1,7 @@
 package me.pljr.pljrapispigot.utils;
 
-import com.sainttx.holograms.api.Hologram;
-import com.sainttx.holograms.api.HologramManager;
-import com.sainttx.holograms.api.HologramPlugin;
-import com.sainttx.holograms.api.line.TextLine;
+import com.gmail.filoghost.holographicdisplays.api.Hologram;
+import com.gmail.filoghost.holographicdisplays.api.HologramsAPI;
 import me.pljr.pljrapispigot.PLJRApiSpigot;
 import me.pljr.pljrapispigot.config.Settings;
 import org.bukkit.Bukkit;
@@ -12,11 +10,10 @@ import org.bukkit.Location;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.List;
-import java.util.UUID;
 
 public final class HologramsUtil {
+    private final static JavaPlugin PLUGIN = PLJRApiSpigot.get();
     private final static Settings SETTINGS = PLJRApiSpigot.get().getSettings();
-    private final static HologramPlugin HOLOGRAM_PLUGIN = JavaPlugin.getPlugin(HologramPlugin.class);
 
     /**
      * Creates a {@link Hologram} for specified time using HolographicDisplays API.
@@ -27,15 +24,17 @@ public final class HologramsUtil {
      */
     public static void create(Location location, List<String> text, int ticks){
         if (!SETTINGS.isHolograms()){
-            Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "PLJRApi: Tried to create a Holograms Hologram while disabled in config!");
+            Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "PLJRApi: Tried to create a HolographicDisplays Hologram while disabled in config!");
             return;
         }
-        HologramManager hologramManager = HOLOGRAM_PLUGIN.getHologramManager();
-        Hologram hologram = new Hologram(UUID.randomUUID().toString(), location);
+        Hologram hologram = HologramsAPI.createHologram(PLUGIN, location);
         for (String line : text){
-            hologram.addLine(new TextLine(hologram, line));
+            hologram.appendTextLine(line);
         }
-        hologramManager.addActiveHologram(hologram);
-        Bukkit.getScheduler().runTaskLaterAsynchronously(PLJRApiSpigot.get(), () -> hologramManager.deleteHologram(hologram), ticks);
+        Bukkit.getScheduler().runTaskLaterAsynchronously(PLUGIN, () -> {
+            if (hologram != null && !hologram.isDeleted()){
+                hologram.delete();
+            }
+        }, ticks);
     }
 }
